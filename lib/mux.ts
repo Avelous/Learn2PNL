2bbc2e2bf356c2085adf3c575d1b3db7529e7c32
avelous <avelousujiri@gmail.com>
2025-01-05 18:41:55 +0100

    muxplayer and aws

--- lib/mux.ts
+++ lib/mux.ts
@@ -3,9 +3,11 @@ import Mux from "@mux/mux-node";
 export const mux = new Mux({
   tokenId: process.env.MUX_TOKEN_ID!,
   tokenSecret: process.env.MUX_TOKEN_SECRET!,
+  jwtSigningKey: process.env.MUX_SIGNING_KEY,
+  jwtPrivateKey: process.env.MUX_PRIVATE_KEY,
 });
 
-export const generatePlaybackToken = async (playbackId: string ) => {
+export const generatePlaybackToken = async (playbackId: string) => {
   const playbackToken = await mux.jwt.signPlaybackId(playbackId, {
     type: "video",
     keyId: process.env.MUX_SIGNING_KEY,
